132df64fe2a1340382748dfd24c8171776915860
Jonas Claes <jonas@jonasclaes.be>
2024-02-21 13:49:41 +0100

    add example navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
+	import MainNav from '$lib/components/main-nav.svelte';
 	import '../app.pcss';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<div class="app">
+<div class="hidden flex-col md:flex">
+	<div class="border-b">
+		<div class="flex h-16 items-center px-4">
+			<MainNav />
+		</div>
+	</div>
 	<main>
 		<slot />
 	</main>

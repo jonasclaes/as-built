40ff250b0d7aff5dd754d08e7b4a3cb4bf5d44a7
Jonas Claes <jonas@jonasclaes.be>
2024-02-22 14:25:57 +0100

    add example cards

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Card, CardHeader, CardTitle, CardContent } from '$lib/components/ui/card';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
@@ -7,5 +8,45 @@
 <section class="flex-1 space-y-4 p-8 pt-6">
 	<div class="flex items-center justify-between space-y-2">
 		<h2 class="text-3xl font-bold tracking-tight">Overview</h2>
+	</div>
+	<div class="space-y-4">
+		<div class="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+			<Card>
+				<CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<CardTitle class="text-sm font-medium">Projects</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div class="text-2xl font-bold">193</div>
+					<p class="text-xs text-muted-foreground">Since the start of your account.</p>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<CardTitle class="text-sm font-medium">Clients</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div class="text-2xl font-bold">97</div>
+					<p class="text-xs text-muted-foreground">Since the start of your account.</p>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<CardTitle class="text-sm font-medium">Project access requests</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div class="text-2xl font-bold">27.482</div>
+					<p class="text-xs text-muted-foreground">Since the start of your account.</p>
+				</CardContent>
+			</Card>
+			<Card>
+				<CardHeader class="flex flex-row items-center justify-between space-y-0 pb-2">
+					<CardTitle class="text-sm font-medium">Project file downloads</CardTitle>
+				</CardHeader>
+				<CardContent>
+					<div class="text-2xl font-bold">12.362</div>
+					<p class="text-xs text-muted-foreground">Since the start of your account.</p>
+				</CardContent>
+			</Card>
+		</div>
 	</div>
 </section>

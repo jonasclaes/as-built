5aca791b9660e56dbfbfe6ad0d40983efe7e2f85
Jonas Claes <jonas@jonasclaes.be>
2024-03-01 23:03:13 +0100

    fix CI config for Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,14 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
+	webServer: process.env.CI
+		? undefined
+		: {
+				command: 'npm run build && npm run preview',
+				port: 4173
+			},
+	use: {
+		baseURL: process.env.BASE_URL
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)(test|spec)\.[jt]s/

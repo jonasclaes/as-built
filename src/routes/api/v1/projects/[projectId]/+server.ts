cf7ffd4ff7ba5e73db01578cf487cdd26e7a2535
Jonas Claes <jonas@jonasclaes.be>
2024-04-24 21:31:36 +0200

    fix: fix project awaits

--- src/routes/api/v1/projects/[projectId]/+server.ts
+++ src/routes/api/v1/projects/[projectId]/+server.ts
@@ -8,7 +8,7 @@ export const GET: RequestHandler = async ({ locals }) => {
 		return errorResponse(new Error('Missing tenant ID.'), 400);
 	}
 
-	const databaseStrategy = getTenantDatabaseStrategy(parseInt(locals.tenantId));
+	const databaseStrategy = await getTenantDatabaseStrategy(parseInt(locals.tenantId));
 	const projectRepository = new ProjectRepository(databaseStrategy);
 
 	const projectsArray = await projectRepository.getProjects();

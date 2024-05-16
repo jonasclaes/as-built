a9e412c75353b62592d071d18def2ad5ddfea1c0
Jonas Claes <jonas@jonasclaes.be>
2024-05-16 22:09:50 +0200

    chore: remove unit tests as we do not use them

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [
-		sveltekit(),
-		viteStaticCopy({
-			targets: [
-				{
-					src: 'drizzle/*',
-					dest: 'drizzle/'
-				}
-			]
-		})
-	],
+	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}'],
 		coverage: {

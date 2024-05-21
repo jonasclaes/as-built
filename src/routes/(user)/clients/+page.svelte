9c525a93e83d340d2e6149c604d165f122d4ac8b
seppestroobants <s.stroobants@hotmail.com>
2024-05-21 11:26:08 +0200

    feat: ik heb iets opgefuck en kreeg het niet in orde dus ben opnieuw begonnen maar alles werkt wel

--- src/routes/(user)/clients/+page.svelte
+++ src/routes/(user)/clients/+page.svelte
@@ -8,4 +8,24 @@
 	<div class="flex items-center justify-between space-y-2">
 		<h2 class="text-3xl font-bold tracking-tight">Clients</h2>
 	</div>
+	<table>
+		<thead>
+			<tr>
+				<th> Client ID </th>
+				<th> Client Name </th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.clients as client}
+				<tr>
+					<td>
+						{client.id}
+					</td>
+					<td>
+						{client.name}
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
 </section>
